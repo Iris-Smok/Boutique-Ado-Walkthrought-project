f0bc9f2b5ff3e2b5544b55f4d5cc3913c148d043
Iris-Smok <irissmok11@gmail.com>
2022-06-07 16:33:41 +0000

    Added sizes to product model and isze selector box to product template

--- bag/contexts.py
+++ bag/contexts.py
@@ -22,7 +22,6 @@ def bag_contents(request):
             'product': product,
         })
 
-
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total

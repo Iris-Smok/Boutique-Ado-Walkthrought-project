018b1d7063a3266f6c86d98856803b039506bbc3
Iris-Smok <irissmok11@gmail.com>
2022-06-15 14:44:32 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LAYkKAJ6jTyqAa7UwwUzvnA0r2LOS5uk7ZFLl8pR4wMLpNAmCOMldIlcfOFj6N6bw8tLNRnUweleWxaJHByt5gK009JCMFf7p',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

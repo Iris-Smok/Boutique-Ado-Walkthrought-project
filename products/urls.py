cd1f5a01c4382c23c22874e002736fdf43c60c07
Iris-Smok <irissmok11@gmail.com>
2022-06-22 14:30:12 +0000

    Securing the views

--- products/urls.py
+++ products/urls.py
@@ -1,3 +1,4 @@
+""" urls for products tempaltes"""
 from django.urls import path
 from . import views
 
@@ -6,5 +7,6 @@ urlpatterns = [
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('delete/<int:product_id>/',
+         views.delete_product, name='delete_product'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 """ views for profile temaplate """
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -8,6 +9,7 @@ from .forms import UserProfileForm
 from checkout.models import Order
 
 
+@login_required()
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
